b609c9d0c6ba358939fa8870e5da39f31e5cc765
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-01-21 23:10:39 +0700

    Test connect to git

--- chapter_001/src/main/java/ru/job4j/Main.java
+++ chapter_001/src/main/java/ru/job4j/Main.java
@@ -1,7 +1,13 @@
 package ru.job4j;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello job4j!");
+        System.out.printf("Test connect to git");
+
     }
+
+
 }
