73c05dda89ad5e8e72c36f076bea640c8ea50aba
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-01-22 23:57:29 +0700

    Нахождение максимального числа

---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+
+    @Test
+    public  void  whenMax1To2Then2(){
+        int result = Max.max(1,2);
+        assertThat(result,is(2));
+    }
+    @Test
+    public  void  whenMax10To2Then10(){
+        int result = Max.max(10,2);
+        assertThat(result,is(10));
+    }
+    @Test
+    public  void  whenMax10To10Then10(){
+        int result = Max.max(10,10);
+        assertThat(result,is(10));
+    }
+}
