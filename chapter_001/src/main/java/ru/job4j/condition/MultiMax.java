3b08c874866e7d021ac62b0d138ca7834e0e4862
Nikolay-Goltsov <n.goltsov@yandex.ru>
2020-01-24 22:01:44 +0700

    Исправил с использованием тернарного оператора

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -1,29 +1,19 @@
 package ru.job4j.condition;
-/*
-4.4. Максимум из трех чисел[#223361]
-Внутри метода max нужно сравнить попарно первое со вторым и максимум из них сравнить с третьим числом.
-В методе нужно использовать несколько раз тернарный оператор условия.
- */
+
 public class MultiMax {
     public  int max(int first, int second, int third){
-        int x = 0;
-        if (first>second){
-             x=first;
-        }else {
-            x=second;
-        }
+        //int x = 0;
+        int x = first>second? first:second;
+      //  if (first>second){
+        //     x=first;
+        //}else {
+          //  x=second;
+        //}
+        int result = third>x?third:x;
+        //if (third>x){
+         //   x=third;
+       // }
+           return result;
 
-
-        if (third>x){
-            x=third;
-        }else {
-            return x;
-        }
-
-       int result= x;
-
-
-
-      return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -6,8 +6,8 @@ public class MultiMaxTest {
     @Test
     public void whenSecondMax() {
         MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 20);
-        assertThat(result, is(20));
+        int result = check.max(10, 4, 2);
+        assertThat(result, is(10));
     }
 
 }
