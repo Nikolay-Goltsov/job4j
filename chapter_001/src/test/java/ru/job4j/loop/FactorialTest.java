c38bd61799fb1a90e1dfae8f7c0c7ceda910c761
Nikolay-Goltsov <n.goltsov@yandex.ru>
2020-01-25 23:20:36 +0700

    Добавлен класс для нахождения фактириала числа

---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        //напишите здесь тест, проверяющий, что факториал для числа 5 равен 120.
+    int result = Factorial.calc(5);
+    int expected = 120;
+    assertThat(result,is(expected));
+    }
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int resilt = Factorial.calc(0);
+        int expercted = 1;
+        assertThat(resilt,is(expercted));
+
+    }
+}
