2a7693e0a0a3989b5fb0887d6205717f65f894fa
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-01-28 00:18:13 +0700

    Типа Фитненс)

---
+++ chapter_001/src/test/java/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+import  org.junit.Test;
+import  static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik(){
+        int mouth = Fitness.calc(95,90);
+        assertThat(mouth,is(0));
+    }
+    @Test
+    public void whenIvanLessBuNik(){
+        int mouth = Fitness.calc(90,95);
+        assertThat(mouth,is(1));
+    }
+    @Test
+    public void whenIvanLessBuFewNik(){
+        int mouth = Fitness.calc(50,90);
+        assertThat(mouth,is(2));
+    }
+
+
+}
