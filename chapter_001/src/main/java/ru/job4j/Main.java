edd682a554f6e41bb80bbf8d2d5c689e53566576
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-01-28 00:28:25 +0700

    Revert "Слил хлам"
    This reverts commit 21757e1a

--- chapter_001/src/main/java/ru/job4j/Main.java
+++ chapter_001/src/main/java/ru/job4j/Main.java
@@ -4,8 +4,8 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello job4j!");
-        System.out.printf("Test connect to git");
+       System.out.println("Hello job4j!");
+       System.out.printf("Test connect to git");
 
     }
 
