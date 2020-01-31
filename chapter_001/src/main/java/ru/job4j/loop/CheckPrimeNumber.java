37322cbc230de9a8b762b9d2805a4137a759bb1c
Nikolay-Goltsov <n.goltsov@yandex.ru>
2020-02-01 00:33:56 +0700

    Исправил код на нахождение простого числа

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,7 +3,8 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int n) {
         boolean prime = true;
-        for (int i=2; i<=n/2; i++) {
+        prime = n != 1;
+        for (int i = 2; i <= n / 2; i++) {
             int temp = n % i;
             if (temp == 0) {
                 prime = false;
