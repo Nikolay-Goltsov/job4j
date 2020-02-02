e75eadb5fc0590eb142b7174ce435c20d912ab0d
Nikolay-Goltsov <n.goltsov@yandex.ru>
2020-02-02 15:05:40 +0700

    Поиски элемента по в массиве

---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenAraayHasnot6() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3};
+        int value = 6;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        //либо
+//        expect=-1;
+        // и тест проходит без ошибок.
+        assertThat(result, is(expect));
+    }
+
+
+}
