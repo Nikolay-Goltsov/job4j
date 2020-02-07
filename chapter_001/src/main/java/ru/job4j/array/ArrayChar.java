e90a5cfac6b4b477992509590feef9d974c7ecdd
Nikolay-Goltsov <n.goltsov@yandex.ru>
2020-02-07 23:42:52 +0700

    Сравнение 2-х массивов символьных(new)

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,12 +4,12 @@ public class ArrayChar {
     public static boolean startsWith(char[] world, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            for (int j = 0; j < world.length; j++) {
-                if (pref[i] != world[i]) {
-                    result = false;
-                    break;
-                }}}
+
+            if (pref[i] != world[i]) {
+                result = false;
+                break;
+            }}
         return result;
-    }
+}
 }
 
