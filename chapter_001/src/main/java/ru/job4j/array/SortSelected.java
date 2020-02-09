79beacac3f3d03d634dff6370b00a7983985885a
Nikolay-Goltsov <n.goltsov@yandex.ru>
2020-02-09 23:51:26 +0700

    Мусор, на хранение

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -3,7 +3,11 @@ package ru.job4j.array;
 public class SortSelected {
     public static int[] sort(int[] data) {
         int min = MinDiapason.findMin(data, 0, data.length);
+        System.out.println("min= " +min);
+
         int index = FindLoop.indexOf(data, min, 0, data.length);
+        System.out.println("min_2= " +min);
+        System.out.println("index= " +index);
         return data;
     }
 }
