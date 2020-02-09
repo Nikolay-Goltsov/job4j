528611a5281b4a86bb8521145dad5cf51723da41
Nikolay-Goltsov <n.goltsov@yandex.ru>
2020-02-09 19:03:33 +0700

    Сравнение символов по строчно (векртикально). На наличее строки с одинаковыми символами

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,14 +3,26 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int rom) {
         boolean result = true;
-
-        for (int i = 0; i <board.length; i++) {
-            for (int j = 0; j <board.length; j++) {
-                if (board[rom][j]!='X') {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[rom][j] != 'X') {
                     result = false;
                     break;
                 }
             }
-        } return result;
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(char[][] board, int columm) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][columm] != 'X') {
+                    result = false;
+                }
+            }
+        }
+        return result;
     }
 }
