1e534656d2a299e8869ac412a5bf3673a0cdc2d0
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-03-17 23:05:49 +0700

    add object Can and Dog

---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog bobik = new Dog();
+        Dog lera = new Dog();
+        Dog dog = new Dog();
+    }
+}
