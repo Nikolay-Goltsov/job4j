164ba41d39fb0fffa5be8b4edfc103dda8b2fa94
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-03-17 23:57:20 +0700

    return String

--- chapter_001/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,15 @@
 package ru.job4j.oop;
 
 public class Cat {
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
     public static void main(String[] args) {
         Cat kitana = new Cat();
         Cat mursyk = new Cat();
+        String say = kitana.sound();
+        System.out.println("Kitana says " + say);
     }
 }
