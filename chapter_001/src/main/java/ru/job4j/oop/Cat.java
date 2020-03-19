c798edc39aa96643ae568d50a9022fc21fe56e42
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-03-19 21:06:50 +0700

    Поля объекта[#223523]

--- chapter_001/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -1,6 +1,21 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
@@ -10,6 +25,22 @@ public class Cat {
         Cat kitana = new Cat();
         Cat mursyk = new Cat();
         String say = kitana.sound();
+        kitana.giveNick("Kitana");
+        System.out.println("There are kitana's food");
+        kitana.eat("milk");
+        kitana.show();
         System.out.println("Kitana says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("chili");
+        gav.show();
+        System.out.println("There are black's food");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fich");
+        black.show();
+
+
     }
 }
