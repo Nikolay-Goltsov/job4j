1b3e9475b63ef2e0f9adb2736e454b6daaa4f98a
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-03-21 16:12:04 +0700

    Рефакторинг - Расстояние между точками.[#223518]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,21 +1,26 @@
 package ru.job4j.condition;
 
-/*
-Задание
-
-1. Создайте класс ru.job4j.condition.Point. Допишите метод distance.
-2. Доработайте метод main Добавьте в него вызов метода distance с различными параметрами точек.
-
-Math.sqrt(a) - корень квадратный от a.
-Math.pow(a, b) - возведение числа а в степень b.
- */
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public static double distance(int x1, int j1, int x2, int j2) {
+        return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((j1 - j2), 2));
+    }
+
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2));
     }
 
     public static void main(String[] args) {
-        double result = distance(12, 45, 23, 20);
-        System.out.println("result (12, 45) to (23, 20) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
