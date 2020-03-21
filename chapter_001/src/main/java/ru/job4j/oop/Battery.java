e39eaaba3a364077de0e7e10481337ee45e716da
Nikolay Goltsov <n.goltsov@yandex.ru>
2020-03-21 19:32:12 +0700

    Состояние объекта.[#223528] Добавил вторую батарекйку

--- chapter_001/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_001/src/main/java/ru/job4j/oop/Battery.java
@@ -9,15 +9,15 @@ public class Battery {
 
     public void exchange(Battery another) {
         this.load = this.load + another.load;
+        another.load = 0;
     }
 
     public static void main(String[] args) {
         Battery bat = new Battery(10);
-        System.out.println("Bat load: " + bat.load);
-        bat.exchange(bat);
-        System.out.println("Bat load: " + bat.load);
-        bat.exchange(bat);
-        System.out.println("Bat load: " + bat.load);
+        Battery batTab = new Battery(23);
+        System.out.println("Bat load: " + bat.load + ", batTab: " + batTab.load);
+        bat.exchange(batTab);
+        System.out.println("Bat load: " + bat.load + ", batTab: " + batTab.load);
     }
 
 }
